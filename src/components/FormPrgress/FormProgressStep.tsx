312a9edd85da004690faa4a60b6ff3f5a6de695b
Atharva Shandilya <atharvashandilya887@gmail.com>
2025-05-23 03:10:12 +0530

    Add transition in form progress step

--- src/components/FormPrgress/FormProgressStep.tsx
+++ src/components/FormPrgress/FormProgressStep.tsx
@@ -12,7 +12,7 @@ export default ({
   return (
     <div className="w-fit flex flex-col items-center mr-[20px]  sm:mr-[112px] last-of-type:mr-0 relative ">
       <p
-        className={` relative bg-[#D9D9D9] rounded-full w-[20px] h-[20px] flex items-center justify-center text-xs mb-2 ${
+        className={` relative bg-[#D9D9D9] transition-colors rounded-full w-[20px] h-[20px] flex items-center justify-center text-xs mb-2 ${
           pageNumber >= count ? "bg-[#FF9000] text-white" : ""
         }`}
       >
@@ -25,7 +25,7 @@ export default ({
 
       {isNext && (
         <div
-          className={` w-[100px] h-[2px] absolute top-[7px] left-[30px] rounded-full hidden sm:block ${
+          className={` w-[100px] h-[2px] absolute top-[7px] left-[30px] rounded-full hidden sm:block transition-colors ${
             pageNumber - 1 >= count ? "bg-[#FF9000]" : "bg-[#D9D9D9]"
           } `}
         ></div>
